561ae3c97960a0335f86e9e42355417715b033e7
Kaiky Guizelini Santos da Silva <kaikyguizelinisantosdasilva@gmail.com>
2022-01-10 17:38:32 -0300

    Redução no tamanho das imagens e video e alterações de cor.

--- scripts/menu-topo.js
+++ scripts/menu-topo.js
@@ -5,11 +5,11 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "0px";
-    document.getElementById("logo").style.height = "15px";
+    document.querySelector("#navbar #logo img").style.height = "15px";
     alterarFonte("12px");
   } else {
     document.getElementById("navbar").style.padding = "10px";
-    document.getElementById("logo").style.height = "26px";
+    document.querySelector("#navbar #logo img").style.height = "26px";
     alterarFonte("14px");
   }
 }
